938935bc62d82a2d743baaff3ce28f2af0fb19e9
jmyoo <yoojaemyeong@gmail.com>
2024-03-22 11:44:15 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox 
+from PyQt5.QtGui import QIcon
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-
     def initUI(self):
-        self.setWindowTitle('Calculator')
+        self.btn1=QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
+        vbox=QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        self.setLayout(vbox)
+        self.setWindowIcon(QIcon('calc.png'))
+         
+        self.setWindowTitle('계산기')
         self.resize(256,256)
         self.show()
-
-
+    def activateMessage(self):
+        QMessageBox.information(self, "information","Button Clicked")
 if __name__=='__main__':
     app = QApplication(sys.argv)
     view = Calculator()
